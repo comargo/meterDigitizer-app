b4abb166a90810c396b4830c814bc5e6e4c6553d
Cyril Margorin <cyril@imperx.com>
2017-08-01 17:44:51 +0300

    Add zeroes for times when there was no data

--- diffirentiate_data.py
+++ diffirentiate_data.py
@@ -1,8 +1,17 @@
+from datetime import datetime, timedelta
+
+def time_generator(start, stop, delta):
+    curTime = start
+    while curTime < stop:
+        yield curTime
+        curTime += delta
+
 class Differentiator(object):
     """docstring for Differentiator."""
 
-    def __init__(self):
+    def __init__(self, delta):
         super(Differentiator, self).__init__()
+        self._delta = delta
         self._min = None
         self._max = None
         self._curTs = None
@@ -11,13 +20,17 @@ class Differentiator(object):
     def ProcessData(self, timestamp, value):
         if not self._curTs:
             #First iteration
-            self._min = self._max = value
+            self._min = value
             self._curTs = timestamp
         elif self._curTs != timestamp:
             #New time
             self.data.append({'timestamp': self._curTs, 'value': self._max - self._min})
-            self._curTs = timestamp
-            self._min = self._max
+            # Append zero values for others
+            if timestamp:
+                for time in time_generator(self._curTs+self._delta, timestamp, self._delta):
+                    self.data.append({'timestamp': time, 'value': 0})
+                self._curTs = timestamp
+                self._min = self._max
         #Current minute
         self._max = value
 
@@ -26,8 +39,8 @@ class Differentiator(object):
 
 def differentiate_data(rows):
     data = {
-        'minute' : Differentiator(),
-        'hour' : Differentiator()
+        'minute' : Differentiator(timedelta(minutes=1)),
+        'hour' : Differentiator(timedelta(hours=1))
     }
 
     for row in rows:
